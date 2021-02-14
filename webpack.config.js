f3af351a742019cec78a3118f8d11059b9bb6838
Naoya Yamashita <conao3@gmail.com>
2021-02-15 05:33:27 +0900

    add files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/assets/js/app.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'js/app.js'
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: 'babel-loader'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  }
+};
