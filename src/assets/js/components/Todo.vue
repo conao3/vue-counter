a079ffc64a37b01354af685d60bde3c694f0bcb4
Naoya Yamashita <conao3@gmail.com>
2021-02-15 19:37:52 +0900

    add Todo vue

---
+++ src/assets/js/components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="item" />
+    <button v-on:click="add_item()">Add Item</button>
+    <ol><li v-for="(it,idx) in items">
+      {{ it }} <button v-on:click="del_item( idx )"> x </button>
+    </li></ol>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [ 'aaa', 'bbb', 'ccc', ],
+        item: 'todo',
+      }
+    },
+    methods: {
+      add_item: function () { this.items.push( this.item ) },
+      del_item: function ( _idx ) { this.items.splice( _idx, 1 ) }
+    }
+  };
+</script>
